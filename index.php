d7893932c21dc10c9e6e85e333473ece03e20fb8
github-tony <tttwww501@gmail.com>
2019-01-16 14:50:43 +0800

    cc

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-echo "Hello 444-live  World!";
-
-echo "Hello 444-77777 World!";
+echo "Hello 444-live  ---  World!";
 
 
+echo "    Hello last --- World!";
 
-echo "Hello 444-3 77777 World!";
-
-
-echo "Hello 444-4 77777 World!";
-
-echo "Hello 444-5 77777 World!";
-
-
-?>
